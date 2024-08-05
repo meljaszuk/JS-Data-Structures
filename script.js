4d7f6c064fd4b925c4ec52b4b793a23dba6cf024
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-08-05 19:40:26 +0200

    add linear search

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('test')
+//Linear search
+
+
+function linearSearch(arr, val) {
+    console.log('LINEAR SEARCH')
+    for (let i=0; i < arr.length; i++) {
+        if (arr[i] === val) {
+            console.log(arr,val,i)
+            return i;
+        }      
+    }
+    
+    console.log(arr,val,-1)
+    
+    return -1;
+}
+
+linearSearch([34,51,1,2,3,45,56,687], 100)
+linearSearch([34,51,1,2,3,45,56,687], 56)
